04407e3a61a3664c4c678e1e1f710e65029ea673
kartik <vermakartik222@gmail.com>
2019-03-24 20:11:00 +0530

    change app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var express = require("express"),
     LocalStrategy = require("passport-local"),
     flash = require("connect-flash");
 
-mongoose.connect(process.env.DATABASEURL, {
+mongoose.connect("mongodb+srv://kartik:kartik@yelcamp-ekpzi.mongodb.net/test?retryWrites=true", {
     useNewUrlParser: true
 });
 
